24eecb7b02076b034be24cf266856e44d6a02dbf
imcvampire <quocanh96@gmail.com>
2021-05-08 08:40:51 +0700

    feat(datasource): post value

--- kamatera/data_source_server.go
+++ kamatera/data_source_server.go
@@ -106,3 +106,22 @@ func dataSourceServer() *schema.Resource {
 		},
 	}
 }
+
+type CreateServerPostValues struct {
+	Name             string `json:"name"`
+	Password         string `json:"password"`
+	PasswordValidate string `json:"passwordValidate"`
+	SshKey           string `json:"ssh-key"`
+	Datacenter       string `json:"datacenter"`
+	Image            string `json:"image"`
+	Cpu              string `json:"cpu"`
+	Ram              int64  `json:"ram"`
+	Disk             string `json:"disk"`
+	DailyBackup      string `json:"dailybackup"`
+	Managed          string `json:"managed"`
+	Network          string `json:"network"`
+	Quantity         string `json:"quantity"`
+	BillingCycle     string `json:"billingcycle"`
+	MonthlyPackage   string `json:"monthlypackage"`
+	PowerOn          string `json:"poweronaftercreate"`
+}
