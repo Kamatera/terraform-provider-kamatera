d8d1c7cabe78969ad4f281f9e7459c36fa1183e8
imcvampire <quocanh96@gmail.com>
2021-05-18 21:13:59 +0700

    fix(*): build request

--- kamatera/resource_server.go
+++ kamatera/resource_server.go
@@ -191,7 +191,7 @@ func resourceServerCreate(ctx context.Context, d *schema.ResourceData, m interfa
 	{
 		diskSizes := d.Get("disk_sizes_gb").([]interface{})
 		for _, v := range diskSizes {
-			diskSizesGB = append(diskSizesGB, fmt.Sprintf("size=%s", v))
+			diskSizesGB = append(diskSizesGB, fmt.Sprintf("size=%v", v))
 		}
 	}
 
