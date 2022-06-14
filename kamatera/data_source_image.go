a6d701e37495b079b6a8e6c8fbfc790de3de9878
Ori Hoch <ori@uumpa.com>
2022-06-14 09:51:30 +0300

    add private_image_name attribute to kamatera_image data resource

--- kamatera/data_source_image.go
+++ kamatera/data_source_image.go
@@ -30,6 +30,10 @@ func dataSourceImage() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
+			"private_image_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
 		},
 	}
 }
@@ -69,50 +73,63 @@ func getAvailableImages(images map[string]map[string]string) string {
 }
 
 func dataSourceImageRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	provider := m.(*ProviderConfig)
-	result, err := request(provider, "GET", fmt.Sprintf("service/server?images=1&datacenter=%s", d.Get("datacenter_id").(string)), nil)
-	if err != nil {
-		d.SetId("")
-		return diag.FromErr(err)
-	}
-	images := map[string]map[string]string{}
-	for _, image := range result.([]interface{}) {
-		images[image.(map[string]interface{})["id"].(string)] = map[string]string{
-			"os":   image.(map[string]interface{})["os"].(string),
-			"code": image.(map[string]interface{})["code"].(string),
-			"name": image.(map[string]interface{})["name"].(string),
-		}
-	}
 	id := d.Get("id").(string)
+	datacenterId := d.Get("datacenter_id").(string)
 	os := d.Get("os").(string)
 	code := d.Get("code").(string)
-	image, hasImage := images[id]
-	osImageIds := getImageMatchesBy(images, "os", os)
-	codeImageIds := getImageMatchesBy(images, "code", code)
-	if hasImage &&
-		(len(osImageIds) == 0 || (len(osImageIds) == 1 && osImageIds[0] == image["id"] && image["os"] == os)) &&
-		(len(codeImageIds) == 0 || (len(codeImageIds) == 1 && codeImageIds[0] == image["id"] && image["code"] == code)) {
-		d.SetId(image["id"])
-		d.Set("code", image["code"])
-		d.Set("os", image["os"])
-		return nil
-	} else if len(osImageIds) == 1 &&
-		(!hasImage || image["os"] == os) &&
-		(len(codeImageIds) == 0 || (len(codeImageIds) == 1 && codeImageIds[0] == osImageIds[0])) {
-		d.SetId(osImageIds[0])
-		d.Set("code", images[osImageIds[0]]["code"])
-		d.Set("os", os)
-		return nil
-	} else if len(codeImageIds) == 1 &&
-		(!hasImage || image["code"] == code) {
-		d.SetId(codeImageIds[0])
-		d.Set("code", code)
-		d.Set("os", images[codeImageIds[0]]["os"])
-		return nil
+	privateImageName := d.Get("private_image_name").(string)
+	if privateImageName == "" {
+		provider := m.(*ProviderConfig)
+		result, err := request(provider, "GET", fmt.Sprintf("service/server?images=1&datacenter=%s", datacenterId), nil)
+		if err != nil {
+			d.SetId("")
+			return diag.FromErr(err)
+		}
+		images := map[string]map[string]string{}
+		for _, image := range result.([]interface{}) {
+			images[image.(map[string]interface{})["id"].(string)] = map[string]string{
+				"os":   image.(map[string]interface{})["os"].(string),
+				"code": image.(map[string]interface{})["code"].(string),
+				"name": image.(map[string]interface{})["name"].(string),
+			}
+		}
+		image, hasImage := images[id]
+		osImageIds := getImageMatchesBy(images, "os", os)
+		codeImageIds := getImageMatchesBy(images, "code", code)
+		if hasImage &&
+			(len(osImageIds) == 0 || (len(osImageIds) == 1 && osImageIds[0] == image["id"] && image["os"] == os)) &&
+			(len(codeImageIds) == 0 || (len(codeImageIds) == 1 && codeImageIds[0] == image["id"] && image["code"] == code)) {
+			d.SetId(image["id"])
+			d.Set("code", image["code"])
+			d.Set("os", image["os"])
+			return nil
+		} else if len(osImageIds) == 1 &&
+			(!hasImage || image["os"] == os) &&
+			(len(codeImageIds) == 0 || (len(codeImageIds) == 1 && codeImageIds[0] == osImageIds[0])) {
+			d.SetId(osImageIds[0])
+			d.Set("code", images[osImageIds[0]]["code"])
+			d.Set("os", os)
+			return nil
+		} else if len(codeImageIds) == 1 &&
+			(!hasImage || image["code"] == code) {
+			d.SetId(codeImageIds[0])
+			d.Set("code", code)
+			d.Set("os", images[codeImageIds[0]]["os"])
+			return nil
+		} else {
+			d.SetId("")
+			d.Set("code", "")
+			d.Set("os", "")
+			return diag.Errorf("could not find matching image, available public images: \n" +
+				"%s\n\n" +
+				"Private images are not listed, see the following link for details: https://github.com/Kamatera/terraform-provider-kamatera/blob/master/README.md#using-a-private-image", getAvailableImages(images))
+		}
 	} else {
-		d.SetId("")
-		d.Set("code", "")
-		d.Set("os", "")
-		return diag.Errorf("could not find matching image, available images: \n%s", getAvailableImages(images))
+		if code != "" || os != "" {
+			return diag.Errorf("When specifying private_image_name, other attributes must not be set")
+		} else {
+			d.SetId(privateImageName)
+			return nil
+		}
 	}
 }
